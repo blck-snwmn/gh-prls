08ddccceb9154321abd79556bc4a30dd2448d1a7
blck-snwmn <whitesnowmancreator@gmail.com>
2023-05-07 23:19:12 +0900

    Impl extension

--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module github.com/blck-snwmn/gh-prls
 
 go 1.20
 
-require github.com/cli/go-gh v1.2.1
+require (
+	github.com/cli/go-gh/v2 v2.0.0
+	github.com/graphql-go/graphql v0.8.1
+)
 
 require (
 	github.com/cli/safeexec v1.0.0 // indirect
-	github.com/cli/shurcooL-graphql v0.0.2 // indirect
+	github.com/cli/shurcooL-graphql v0.0.3 // indirect
 	github.com/henvic/httpretty v0.0.6 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
--- main.go
+++ main.go
@@ -1,26 +1,64 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 
-	"github.com/cli/go-gh"
+	"github.com/cli/go-gh/v2/pkg/api"
+	graphql "github.com/cli/shurcooL-graphql"
 )
 
 func main() {
-	fmt.Println("hi world, this is the gh-prls extension!")
-	client, err := gh.RESTClient(nil)
+	userName, err := getUserName(context.Background())
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
+
+	client, err := api.DefaultGraphQLClient()
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	fmt.Printf("running as %s\n", response.Login)
+
+	var query struct {
+		Search struct {
+			Nodes []struct {
+				PullRequest struct {
+					Title      string
+					Url        string
+					Repository struct {
+						Name string
+						Url  string
+					}
+				} `graphql:"... on PullRequest"`
+			}
+			IssueCount int
+		} `graphql:"search(first: 100, type: ISSUE, query: $query)"`
+	}
+	variables := map[string]interface{}{
+		"query": graphql.String(fmt.Sprintf("is:open is:pr archived:false user:%s", userName)),
+	}
+	if err := client.Query("search", &query, variables); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(query.Search.IssueCount)
+	for _, node := range query.Search.Nodes {
+		fmt.Println(node.PullRequest.Title)
+		fmt.Println(node.PullRequest.Url)
+		fmt.Println(node.PullRequest.Repository.Name)
+		fmt.Println(node.PullRequest.Repository.Url)
+	}
 }
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+func getUserName(ctx context.Context) (string, error) {
+	client, err := api.DefaultRESTClient()
+	if err != nil {
+		return "", err
+	}
+	response := struct{ Login string }{}
+	err = client.Get("user", &response)
+	if err != nil {
+		return "", err
+	}
+	return response.Login, nil
+}
